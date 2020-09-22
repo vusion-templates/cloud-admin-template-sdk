29b13bb5e90c74703456d61506ec4703a4dd456d
zhangsanshi <zhangsanshi5566@gmail.com>
2020-09-22 17:30:08 +0800

    feat: Add prettier for format

--- src/tests/meta/apollo/dsl-to-all/generator/paramsTansform.test.ts
+++ src/tests/meta/apollo/dsl-to-all/generator/paramsTansform.test.ts
@@ -1,24 +1,23 @@
-
-import { PathToBinaryExpressionString } from '../../../../../lib/meta/apollo/dsl-to-all/generator/paramsTansform';
+// import { PathToBinaryExpressionString } from '../../../../../lib/meta/apollo/dsl-to-all/generator/paramsTansform';
 
 /**
  * 测试
- * asdfaf{as} 
- * 
+ * asdfaf{as}
+ *
  * 转化为 'asdfaf' + args.as
- * 
+ *
  * cmd:  node dist/lib/meta/apollo/dsl-to-all/test/index.js
  */
-function Test(path: string) {
-  const result = PathToBinaryExpressionString(path);
-}
+// function Test(path: string) {
+//   const result = PathToBinaryExpressionString(path);
+// }
 
-Test("asdfaf{as}");
-Test("asdfaf");
-Test("");
+// Test("asdfaf{as}");
+// Test("asdfaf");
+// Test("");
 
-describe('paramsTansform', () => {
-  test('todo', () => {
+describe("paramsTansform", () => {
+  test("todo", () => {
     expect(true).toEqual(true);
   });
 });
