65a911cd1e19dca822ddc78c594ee311f5910756
zhangsanshi <zhangsanshi5566@gmail.com>
2020-10-12 16:00:42 +0800

    fix: designer error

--- template/page/src/index.js
+++ template/page/src/index.js
@@ -2,4 +2,4 @@ import platformConfig from '../../../platform.config.json';
 import appConfig from './app.config';
 import designer from '@/global/features/designer';
 import rootRoute from './routes.map.js?rootPath=/';
-designer(appConfig, platformConfig, rootRoute);
+designer.init(appConfig, platformConfig, rootRoute);
